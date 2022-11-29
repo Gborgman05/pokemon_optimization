2699094f892064e51280788cc5c1146326d79f75
Gborgman05 <gborgman@calpoly.edu>
2022-11-29 08:46:10 -0800

    daily

--- best_pokemon.py
+++ best_pokemon.py
@@ -1,6 +1,26 @@
 from pokemon_type import *
+import pokebase as pb
 def main():
     possible_types = find_best_offensive()
+    pokedex = pb.pokedex('galar')  # Quick lookup.
+    poss = get_poss(pokedex, possible_types[0])
+    print(poss)
+
+
+
+def get_poss(pokedex, possible_types):
+    poss_pokemon = {}
+    count = 0
+    for pokemon in pokedex.pokemon_entries:
+        print(f"pokemon {count}")
+        pokemon = pb.pokemon(pokemon.pokemon_species.name)
+        for ty in pokemon.types:
+            if ty.type in possible_types:
+                poss_pokemon[pokemon.name] = [t.type for t in pokemon.types]
+                break
+    return poss_pokemon
+        
+
     
 
 
