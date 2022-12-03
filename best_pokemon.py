eca5f6165889a5a4e41ea74e11d0c1be8722b0f1
Galen Borgman <gborgman@calpoly.edu>
2022-12-02 16:18:02 -0800

    daily

--- best_pokemon.py
+++ best_pokemon.py
@@ -1,10 +1,19 @@
 from pokemon_type import *
+import pdb
 import pokebase as pb
 def main():
     possible_types = find_best_offensive()
-    pokedex = pb.pokedex('galar')  # Quick lookup.
-    poss = get_poss(pokedex, possible_types[0])
-    print(poss)
+    pokedex = pb.pokedex('national')  # Quick lookup.
+    total_types = []
+    for lst in possible_types:
+        for ty in lst:
+            if ty not in total_types:
+                total_types.append(ty)
+    total_types = [typ.lower() for typ in total_types]
+    poss = get_poss(pokedex, total_types)
+
+    with open("possible_pokemon.json", "w") as f:
+        f.write(str(poss))
 
 
 
@@ -12,11 +21,15 @@ def get_poss(pokedex, possible_types):
     poss_pokemon = {}
     count = 0
     for pokemon in pokedex.pokemon_entries:
-        print(f"pokemon {count}")
+        count += 1
         pokemon = pb.pokemon(pokemon.pokemon_species.name)
+        print(f"pokemon: {pokemon.name}, number {count}")
+        # pdb.set_trace()
+        f = open("possible_pokemon.json", "w+")
         for ty in pokemon.types:
-            if ty.type in possible_types:
-                poss_pokemon[pokemon.name] = [t.type for t in pokemon.types]
+            if ty.type.name in possible_types:
+                f.write 
+                poss_pokemon[pokemon.name] = [t.type.name for t in pokemon.types]
                 break
     return poss_pokemon
         
