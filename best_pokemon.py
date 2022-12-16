6b1e4dbcd3ce5f8ce2c34e8661f2b0a940d02c19
Gborgman05 <gborgman@calpoly.edu>
2022-12-16 13:04:32 -0800

    stuck on evolution chains

--- best_pokemon.py
+++ best_pokemon.py
@@ -11,14 +11,20 @@ def main():
             if ty not in total_types:
                 total_types.append(ty)
     total_types = [typ.lower() for typ in total_types]
-    poss = get_poss(total_types)
+    poss = get_poss(total_types, only_two=True)
 
     with open("possible_pokemon.json", "w") as f:
+        f.write(f"{len(poss)}\n")
         f.write(str(poss))
 
+def filter_by_evolution(poss):
+    """filters dictionary of pokemon by removing lower evolutions"""
+    for name in poss:
+        resp = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}").content)
+        pdb.set_trace()
 
 
-def get_poss(possible_types):
+def get_poss(possible_types, only_two=False):
     with open("download.json") as f:
         pokedex = json.loads(f.read())
     
@@ -30,10 +36,21 @@ def get_poss(possible_types):
         for pokemon in resp["pokemon"]:
             # pdb.set_trace()
             pokemon = pokemon["pokemon"]
+            if "gmax" in pokemon["name"] or "mega" in pokemon["name"]:
+                continue
             if pokemon["name"] in poss:
                 poss[pokemon["name"]].append(typ)
             else:
                 poss[pokemon["name"]] = [typ]
+    if only_two:
+        nw = {}
+        for key in poss:
+            if len(poss[key]) >= 2:
+                nw[key] = poss[key]
+        poss = nw
+        
+    poss = filter_by_evolution(poss)
+                
     return poss
     
             
